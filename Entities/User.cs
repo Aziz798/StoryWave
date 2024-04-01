a12104ec3d3fe44add69214b2ae02460b1ffac4b
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-04-01 18:16:36 +0100

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    internal class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="FirstName is required")]
+        public string FirstName { get; set; }
+    }
+}
